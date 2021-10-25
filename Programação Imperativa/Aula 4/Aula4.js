cf33c947daab84be451cd0e0099c848064db3764
Rafael Queiroz <rafa.qo@gmail.com>
2021-10-25 20:49:44 -0300

    Aula 3 de Programação Imperativa

---
+++ Programação Imperativa/Aula 4/Aula4.js
@@ -0,0 +1,37 @@
+console.log (
+    "Olá, mundo!"
+);
+
+var x = 8; // Atribuição
+// console.log (x);
+
+x = 9; // Atribuição
+// console.log (x);
+
+const y = 6;
+// console.log (y);
+
+let z = 2;
+z = 3;
+// console.log (z);
+
+let soma = x + y + z;
+
+let subtracao =  x-z;
+
+let multiplicacao = x * y;
+
+let divisao = x/z;
+
+// console.log(soma);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+
+let nome = 'Rafael';
+// console.log(nome);
+
+let idade = 35;
+
+console.log(nome, idade);
+console.log(nome+" - "+idade);
---
+++ Programação Imperativa/Aula 4/ex3.js
@@ -0,0 +1,57 @@
+// Exercício 1
+
+console.log( 2 + 3);
+console.log(2*3);
+
+let x = 4;
+let y = 6;
+
+console.log(x+y);
+
+let z = 10;
+
+console.log(x+y+z);
+
+
+// Exercício 2
+let nome, sobrenome, idade, peso, altura, plano, IMC;
+
+nome = 'José';
+sobrenome = 'da Silva';
+idade = 27;
+peso = 83.5;
+altura = 1.70;
+plano = 'Tem';
+IMC = peso / (altura*altura);
+
+console.log (nome+" "+sobrenome+" tem a idade de "+idade+" tem o IMC de "+IMC);
+
+nome = 'Carlos';
+sobrenome = 'de Souza';
+idade = 28;
+peso = 80.1;
+altura = 1.76;
+plano = 'Tem';
+IMC = peso / (altura*altura);
+
+console.log (nome+" "+sobrenome+" tem a idade de "+idade+" tem o IMC de "+IMC);
+
+nome = 'Aline';
+sobrenome = 'Ferreira';
+idade = 33;
+peso = 63.7;
+altura = 1.53;
+plano = 'Não tem';
+IMC = peso / (altura*altura);
+
+console.log (nome+" "+sobrenome+" tem a idade de "+idade+" tem o IMC de "+IMC);
+
+nome = 'Ana';
+sobrenome = 'Paula';
+idade = 26;
+peso = 55;
+altura = 1.62;
+plano = 'Tem';
+IMC = peso / (altura*altura);
+
+console.log (nome+" "+sobrenome+" tem a idade de "+idade+" tem o IMC de "+IMC);

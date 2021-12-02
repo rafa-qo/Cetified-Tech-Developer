95a12553571f159e90bb45c299e3df36cd4af258
Rafael Queiroz <rafa.qo@gmail.com>
2021-12-02 10:31:29 -0300

    exercícios Aula 18 Programação Imperativa 02/12/2021

--- ProgramacaoImperativa/Aula18/exercicio18.js
+++ ProgramacaoImperativa/Aula18/exercicio18.js
@@ -47,12 +47,12 @@ let criaArray = (b) => {
     return soma;
 };
 
-console.log(criaArray(10));
+// console.log(criaArray(10));
 
 // Novo Array
 // Você deve criar uma função chamada newArray que receba um número como parâmetro e retorne um novo array com a quantidade de elementos equivalente ao valor do número que você passou por parâmetro.
 
-newArray = (c) => {
+let newArray = (c) => {
     let array = [];
     let qtd = c;
 
@@ -62,8 +62,88 @@ newArray = (c) => {
     console.log(array);
 };
 
-newArray(5);
+// newArray(5);
 
 // String.split()
 // Você deve criar uma função chamada split que receba uma string e simule o comportamento da função split original. Se você não sabe como funciona, o Google pode ajudá-lo.
 // Importante: Você não pode usar o String.split()
+let split = (p) =>
+{
+    let palavra = [];
+    for (let i = 0; i < p.length; i++) {
+        palavra[i] = p[i];
+    }
+    return palavra;
+}
+
+// console.log(split("Rafael"));
+
+// Você deve criar uma função chamada moverZeros que receba um array como parâmetro e retorne outro com os números 0 ordenados ao final.
+let teste = [false,1,0,1,2,0,1,3,"a"]
+
+let moverZeros = (m) =>
+{
+    let novoArray = m.filter((m) => {
+        return m !== 0;
+    });
+
+    m.forEach(valor => {
+        if (valor == 0) {
+            novoArray.push(valor);
+        }  
+    });
+    return novoArray;
+
+};
+
+// console.log(moverZeros(teste));
+
+// Você deve criar uma função chamada arrayHandler que receba dois arrays de igual tamanho como parâmetros e exiba no console: "Eu sou {elemento do array 1} e eu sou {elemento do array 2}".
+// Exemplo:
+// arrayHandler([1,2,3], [“o”,”l”,”á”]) deve mostrar: 
+// 		Eu sou 1 e eu sou o
+// Eu sou 2 e eu sou l
+// Eu sou 3 e eu sou á
+
+function arrayHandler(x, y) {
+    for (let i = 0; i < x.length; i++) {
+        console.log("Eu sou " + x[i] + " e eu sou " + y[i]);
+    }
+}
+
+// arrayHandler([1,2,3], ['o','l','á']);
+
+
+// Você deve criar uma função chamada arrayObjects que receba um número como parâmetro e retorne um array de objetos que tem uma propriedade chamada valor que contém o valor do número e seus anteriores.
+// Exemplo:
+// arrayObjects(5) deve retornar [{valor: 1}, {valor: 2}, {valor: 3}, {valor: 4}, {valor: 5}]
+// arrayObjects(3) deve retornar [{valor: 1}, {valor: 2}, {valor: 3}]
+
+let arrayObjects = (num) => 
+{
+    let array = [];
+    for (let i = 0; i < num; i++) {
+        array.push({valor:[i]});
+    }
+    return array;
+}
+
+// console.log(arrayObjects(5));
+
+// Arrays de objetos 2
+// Você deve criar uma função chamada arrayObjectsTwo que recebe um número e uma palavra como parâmetro e retorna um array de objetos que possui: uma propriedade nomeada pela palavra passada por parâmetro e o valor dessa propriedade deve ser o número passado por parâmetro e seus anteriores.
+
+// Exemplo:
+// arrayObjectsTwo(5, “olá”) deve retornar [{olá: 1}, {olá: 2}, {olá: 3}, {olá: 4}, {olá: 5 }]
+// arrayObjectsTwo(3, “tchau”) deve retornar [{tchau: 1}, {tchau: 2}, {tchau: 3}]
+
+let arrayObjectsTwo = (num2, p) => 
+{
+    let array2 = [];
+    for (let i = 0; i < num2; i++) {
+        array2.push({[p]:[i]});
+    }
+    return array2;
+}
+
+// console.log(arrayObjectsTwo(5, "teste"));

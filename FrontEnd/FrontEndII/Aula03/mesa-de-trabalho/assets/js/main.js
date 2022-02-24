6b6b7b150d751003101af87f8099efaf8cdeffa0
Rafael Queiroz <rafa.qo@gmail.com>
2022-02-23 22:10:58 -0300

    Arrumando os arquivos

---
+++ FrontEnd/FrontEndII/Aula03/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,3 @@
+document.querySelector("#linha-2_coluna-1").innerHTML = "O";
+document.querySelector("#linha-1_coluna-1").innerHTML = "X";
+document.querySelector("#linha-3_coluna-1").innerHTML = "O";

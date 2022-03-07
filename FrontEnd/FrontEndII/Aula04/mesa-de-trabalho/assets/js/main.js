2d40e3affb3b7c252159c18846fc6daf0481b89e
Rafael Queiroz <rafa.qo@gmail.com>
2022-03-07 20:20:32 -0300

    Exercício Aula04 Front End II

---
+++ FrontEnd/FrontEndII/Aula04/mesa-de-trabalho/assets/js/main.js
@@ -0,0 +1,22 @@
+// Nome do grupo: 
+// Rafael Vasconcelos
+// Rafael Queiroz
+// Rafael Barbosa Simões
+// Raphael Dutra
+// Gustavo "Rafael" Toffetti
+
+
+let selectLista = document.querySelectorAll(".lista__item");
+
+// console.log(select);
+
+selectLista[0].classList.add("removido");
+selectLista[0].innerText = "Carro - Removido";
+selectLista[1].classList.add("selecionado");
+selectLista[1].innerText = "Bike - Selecionado";
+selectLista[2].classList.add("selecionado");
+selectLista[2].innerText = "Patinete - Selecionado";
+selectLista[3].classList.add("removido");
+selectLista[3].innerText = "Celular - Removido";
+selectLista[4].classList.add("removido");
+selectLista[4].innerText = "Carregador - Removido";

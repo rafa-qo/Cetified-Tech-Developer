c2a69ace93834e162f54514f07217d792931014a
Rafael de Queiroz Oliveira <34455788+rafa-qo@users.noreply.github.com>
2022-11-09 22:53:49 -0300

    aula 09/11/2022

--- FrontEnd/FrontEndIII/CheckpointI/src/App.jsx
+++ FrontEnd/FrontEndIII/CheckpointI/src/App.jsx
@@ -1,16 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Card from './Card'
 
 function App() {
+
+  const [formData, setFormData] = useState(
+    {
+      nome: "", 
+      hexadecimal: ""
+    }
+  );
+
+  const [card, setCard] = useState(
+    [
+      {
+        nome: "",
+        hexadecimal: ""
+      }
+    ]
+  );
   
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if(formData.hexadecimal.length == 6 || formData.nome != ""){
+
+      setCard([...card, 
+        {
+          nome: formData.nome, 
+          hexadecimal: formData.hexadecimal
+        }
+      ])
+
+    } else {
+    
+      alert("Formato inválido. Digite o nome da cor e o Hexadecimal com 6 dígitos e sem o #");
+      
+    }
+
+  };
 
   return (
     <div className="App">
-      <h1>Escolha sua cor</h1>
-      <form></form>
-      <Card />
+      <div className='header-form'>
+        <h1>Escolha sua cor</h1>
+        <form>
+          <input className='formInput' value={formData.nome}
+            onChange={(evento)=>setFormData({...formData, nome: evento.target.value})}
+            placeholder='Digite o nome da cor'/>
+          <input className="formInput" value={formData.hexadecimal}
+            onChange={(evento)=>setFormData({...formData, hexadecimal: evento.target.value})}
+            placeholder='Digite o Hexadecimal sem o #'/>
+          <input className='formButton' type="submit" onClick={handleSubmit} />
+        </form>
+      </div>
+
+      <h1>Cores selecionadas</h1>
+
+      {card.map((card)=>{
+        return(
+
+          <Card key={card.hexadecimal} nome={card.nome} hexadecimal={card.hexadecimal}/>
+
+        )
+      })}
+
+    
+
     </div>
   );
 
--- FrontEnd/FrontEndIII/CheckpointI/src/Card.jsx
+++ FrontEnd/FrontEndIII/CheckpointI/src/Card.jsx
@@ -1,11 +1,14 @@
+import './app.css';
+
 function Card(props) {
 
     return(
-        <div>
-            <h1>{props.cor}</h1>
-            <h2>{props.nome}</h2>
+        <div key={props.hexadecimal} className="card">
+            <h1>{props.nome}</h1>
+            <h2>{props.hexadecimal}</h2>
         </div>
-    ); 
+    );
+
 }
 
 export default Card;

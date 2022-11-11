e3393a204921d591d6f9baf2efc69722eae9c57a
Rafael de Queiroz Oliveira <34455788+rafa-qo@users.noreply.github.com>
2022-11-10 22:15:38 -0300

    Checkpoint Front End III - ajustes 10/11/2022

--- FrontEnd/FrontEndIII/CheckpointI/src/Card.jsx
+++ FrontEnd/FrontEndIII/CheckpointI/src/Card.jsx
@@ -2,10 +2,23 @@ import './app.css';
 
 function Card(props) {
 
+    const bgColor = {
+        padding: 30,
+        marginRight: 20,
+        marginBottom: 20,
+        // position: 'relative',
+        float: 'left',
+        backgroundColor: '#'+props.hexadecimal,
+        height: 200,
+        width: 370,
+        borderRadius: 10,
+
+    }
+
     return(
-        <div key={props.hexadecimal} className="card">
+        <div key={props.hexadecimal} style={bgColor}>
             <h1>{props.nome}</h1>
-            <h2>{props.hexadecimal}</h2>
+            <h2>#{props.hexadecimal}</h2>
         </div>
     );
 

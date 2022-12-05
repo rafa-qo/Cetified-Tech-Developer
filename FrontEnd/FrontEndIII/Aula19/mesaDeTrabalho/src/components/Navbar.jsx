4964bfd8a9444d541b4f8e9c0ebcf3a200b9eee5
Rafael de Queiroz Oliveira <34455788+rafa-qo@users.noreply.github.com>
2022-12-04 21:53:47 -0300

    arrumando o git

--- FrontEnd/FrontEndIII/Aula19/mesaDeTrabalho/src/components/Navbar.jsx
+++ FrontEnd/FrontEndIII/Aula19/mesaDeTrabalho/src/components/Navbar.jsx
@@ -4,28 +4,6 @@ import { LanguageContext }  from '../providers/LanguageProvider'
 
 const Navbar = () => {
     const { language, languageID, handleLanguageID } = useContext(LanguageContext);
-
-    function handleLanguage(languageID) {
-        if (languageID === "US") {
-          setLanguage(
-            {
-              title: "Welcome to the translations project from Digital House",
-              description: "You will learn how to use context to make your translations",
-              home: "Home",
-              current: "Current language",
-              button: "Change language"
-            }
-          );
-        } else {
-          setLanguage({
-            title: "Bem-vindo ao projeto de traduções da Digital House",
-            description: "Você aprenderá como usar o contexto para fazer suas traduções",
-            home: "Início",
-            current: "Idioma atual",
-            button: "Trocar Idioma"
-          })
-        }
-      }
     
     return (
         <div className="navbar">
